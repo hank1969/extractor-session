a40e8ee7f54253180390b0d7f87c745848852e8b
Smashingk <smashingk@gmail.com>
2018-04-28 09:46:28 -0700

    Initial :robot:

---
+++ lib/extractor.rb
@@ -0,0 +1,7 @@
+
+module Extractor
+  class NotAuthorizedError < StandardError; end
+  class ProxyError < StandardError; end
+  class InvalidProfileUrlError < StandardError; end
+  class UserVisibilityError < StandardError; end
+end
