235fc09495a549e27ea3ac95f0365823a2d2b742
hank1902 <me@tubrosmedia.com>
2019-05-12 00:02:30 -0700

    Fix bug

--- lib/extractor/session.rb
+++ lib/extractor/session.rb
@@ -375,7 +375,7 @@ module Extractor
     end
 
     def input_value(name, body)
-      %r{<input name="#{name}" value="(.+?)"}.match(response.body)[1]
+      %r{<input name="#{name}" value="(.+?)"}.match(body)[1]
     end
 
     def get_latest_email_challenge_pin(response)
